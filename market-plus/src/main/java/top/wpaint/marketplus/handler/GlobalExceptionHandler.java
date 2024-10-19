bf9bc06942624c96897a907d1b5e6ca4b8fa5daf
tecwds <tecwds@163.com>
2024-10-19 20:47:26 +0800

    add: 应用异常处理

--- market-plus/src/main/java/top/wpaint/marketplus/handler/GlobalExceptionHandler.java
+++ market-plus/src/main/java/top/wpaint/marketplus/handler/GlobalExceptionHandler.java
@@ -3,18 +3,28 @@ package top.wpaint.marketplus.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import top.wpaint.marketplus.common.AppException;
 import top.wpaint.marketplus.entity.ResponseEntity;
 
 @Slf4j
-//@ControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    /**
+     * 应用异常处理
+     */
+    @ExceptionHandler(AppException.class)
+    public ResponseEntity<String> appExceptionHandler(AppException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.error(e.getCode(), e.getMessage());
+    }
 
     /**
      * 通用异常处理
      */
-//    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<String> exceptionHandler(Exception e) {
-//        log.error(e.getMessage());
+        log.error(e.getMessage());
         return ResponseEntity.error();
     }
 }
