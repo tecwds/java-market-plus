da2862e58efce8d8bb81f0f66b9f3370698ed431
tecwds <tecwds@163.com>
2024-10-19 20:47:26 +0800

    add: 应用异常处理

---
+++ market-plus/src/main/java/top/wpaint/marketplus/common/AppException.java
@@ -0,0 +1,44 @@
+package top.wpaint.marketplus.common;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.io.Serial;
+
+@Data
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class AppException extends Exception {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 异常状态码
+     */
+    private Integer code;
+
+    /**
+     * 异常信息
+     */
+    private String message;
+
+    public AppException(Integer code) {
+        this.code = code;
+    }
+
+    public AppException(Integer code, String message, Throwable cause) {
+        super.initCause(cause);
+        this.message = message;
+        this.code = code;
+    }
+
+    public AppException(Integer code, Throwable cause) {
+        super.initCause(cause);
+        this.code = code;
+    }
+
+    public AppException(ResponseStatus status, Throwable cause) {
+        this(status.getCode(), status.getMessage(), cause);
+    }
+}
