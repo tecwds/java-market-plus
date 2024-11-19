eeea7852c66c00a13171f24463aa93a677151340
tecwds <tecwds@163.com>
2024-11-19 08:54:03 +0000

    add: 权限列表

--- market-plus/src/main/java/top/wpaint/marketplus/common/constant/RoleConst.java
+++ market-plus/src/main/java/top/wpaint/marketplus/common/constant/RoleConst.java
@@ -1,5 +1,8 @@
 package top.wpaint.marketplus.common.constant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class RoleConst {
     /**
      * 普通用户
@@ -25,5 +28,20 @@ public final class RoleConst {
     public static final String R_GUEST = "R_GUEST";
     public static final String R_GUEST_DES = "访客";
 
+    /**
+     * 客服
+     */
+    public static final String R_SERVICE = "R_SERVICE";
+    public static final String R_SERVICE_DES = "客服";
 
+    public static final List<String> ROLE_LIST;
+
+    static {
+        ROLE_LIST = new ArrayList<>(5);
+        ROLE_LIST.add("R_USER");
+        ROLE_LIST.add("R_ADMIN");
+        ROLE_LIST.add("R_SELLER");
+        ROLE_LIST.add("R_GUEST");
+        ROLE_LIST.add("R_SERVICE");
+    }
 }
