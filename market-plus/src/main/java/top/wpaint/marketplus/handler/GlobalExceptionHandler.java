6b10e57b4f28c3163802ef7cf95e1657668a7e55
tecwds <tecwds@163.com>
2024-11-19 10:19:26 +0000

    update: 捕获全局异常（Exception）

--- market-plus/src/main/java/top/wpaint/marketplus/handler/GlobalExceptionHandler.java
+++ market-plus/src/main/java/top/wpaint/marketplus/handler/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import ch.qos.logback.core.status.Status;
 import top.wpaint.marketplus.common.exception.AppException;
 import top.wpaint.marketplus.common.Result;
 
@@ -19,5 +21,12 @@ public class GlobalExceptionHandler {
     public Result<String> appExceptionHandler(AppException e) {
         log.error(e.getMessage());
         return Result.error(e.getCode(), e.getMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public Result<String> ExceptionHandler(Exception e) {
+        log.error(e.getMessage());
+        return Result.error(Status.ERROR);
     }
 }
