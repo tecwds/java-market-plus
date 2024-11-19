0c8975f13d02b5781ae5bacf6635a1cdf0512cc3
tecwds <tecwds@163.com>
2024-11-19 09:42:50 +0000

    add: 新增一个商店

---
+++ market-plus/src/main/java/top/wpaint/marketplus/controller/StoreController.java
@@ -0,0 +1,41 @@
+package top.wpaint.marketplus.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.dev33.satoken.annotation.SaCheckLogin;
+import lombok.extern.slf4j.Slf4j;
+import top.wpaint.marketplus.common.Result;
+import top.wpaint.marketplus.common.exception.AppException;
+import top.wpaint.marketplus.entity.dto.StoreDTO;
+import top.wpaint.marketplus.entity.vo.StoreVO;
+import top.wpaint.marketplus.service.StoreService;
+
+@Slf4j
+@SaCheckLogin
+@RestController
+@RequestMapping("/api/store")
+public class StoreController {
+    
+    private final StoreService storeService;
+
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
+    /**
+     * 开一个新店
+     * @return 结果
+     */
+    @PostMapping()
+    public Result<StoreVO> openNewStore(@RequestBody StoreDTO body) throws AppException {
+        log.debug("add new store -- {}", body);
+        return storeService.doOpenNewStore(body);
+    }
+
+    
+
+
+}
