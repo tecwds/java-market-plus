2ca1dd124f47bf2ca06b44fc3dd93f64c2b7cc6c
tecwds <tecwds@163.com>
2024-11-19 08:53:49 +0000

    add: 部分权限

---
+++ market-plus/src/main/java/top/wpaint/marketplus/common/constant/Permission.java
@@ -0,0 +1,24 @@
+package top.wpaint.marketplus.common.constant;
+
+/**
+ * 权限集合，主要针对接口
+ */
+public final class Permission {
+    // HTTP 方法
+    public static final String GET = "http.get";
+    public static final String POST = "http.post";
+    public static final String PUT = "http.put";
+
+    // 添加标签
+    public static final String TAG_ADD = "tag.add";
+
+    // 删除标签
+    public static final String TAG_DEL = "tag.del";
+
+    
+
+
+
+
+    
+}
