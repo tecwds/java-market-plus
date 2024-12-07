3d7a6ec0ab1b3afc5d3075a58917ad335b59b201
tecwds <tecwds@163.com>
2024-12-07 23:54:29 +0800

    add: 查询购物车商品

--- market-plus/src/main/java/top/wpaint/marketplus/entity/vo/CartInfoVO.java
+++ market-plus/src/main/java/top/wpaint/marketplus/entity/vo/CartInfoVO.java
@@ -1,5 +1,6 @@
 package top.wpaint.marketplus.entity.vo;
 
+import com.mybatisflex.annotation.RelationOneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +13,35 @@ import java.math.BigInteger;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class GoodsVO implements Serializable {
-    private BigInteger id;
+public class CartInfoVO implements Serializable {
+
+    /**
+     * 商品 ID
+     */
+    private BigInteger goodsId;
+
+    /**
+     * 商品名称
+     */
     private String name;
+
+    /**
+     * 商品描述
+     */
     private String description;
+
+    /**
+     * 商品图像
+     */
     private String image;
+
+    /**
+     * 价格
+     */
     private BigInteger price;
+
+    /**
+     * 数量（来自 Cart 购物车）
+     */
     private Long count;
 }
--- market-plus/src/main/java/top/wpaint/marketplus/service/CartService.java
+++ market-plus/src/main/java/top/wpaint/marketplus/service/CartService.java
@@ -2,6 +2,10 @@ package top.wpaint.marketplus.service;
 
 import com.mybatisflex.core.service.IService;
 import top.wpaint.marketplus.entity.Cart;
+import top.wpaint.marketplus.entity.vo.CartInfoVO;
+import top.wpaint.marketplus.entity.vo.GoodsVO;
+
+import java.util.List;
 
 /**
  *  服务层。
@@ -12,4 +16,6 @@ import top.wpaint.marketplus.entity.Cart;
 public interface CartService extends IService<Cart> {
 
     void doAddGoodsToCart(String goodsId, Long count);
+
+    List<CartInfoVO> doGetCartInfo();
 }
