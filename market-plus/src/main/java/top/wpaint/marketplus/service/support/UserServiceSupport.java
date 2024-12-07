e2f17f8653082fda9eea98dde0a901612864e0ab
tecwds <tecwds@163.com>
2024-12-07 19:15:00 +0800

    add: 用户开一个店

--- market-plus/src/main/java/top/wpaint/marketplus/service/support/UserServiceSupport.java
+++ market-plus/src/main/java/top/wpaint/marketplus/service/support/UserServiceSupport.java
@@ -1,9 +1,29 @@
 package top.wpaint.marketplus.service.support;
 
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import top.wpaint.marketplus.common.Status;
+import top.wpaint.marketplus.common.UserInfoStorage;
+import top.wpaint.marketplus.common.exception.AppException;
+import top.wpaint.marketplus.entity.User;
+import top.wpaint.marketplus.mapper.UserMapper;
 
+@Slf4j
 @Component
 public class UserServiceSupport {
 
+    @Resource
+    private UserMapper userMapper;
 
+    public User getUserFromStorage() throws AppException {
+        User u = userMapper.selectOneById(UserInfoStorage.getUserId());
+
+        if (null == u) {
+            // 这里可能没有登陆，直接返回未登录
+            throw new AppException(Status.USER_NOT_LOGIN);
+        }
+
+        return u;
+    }
 }
