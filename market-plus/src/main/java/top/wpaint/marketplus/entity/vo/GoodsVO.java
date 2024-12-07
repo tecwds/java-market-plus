a54623ba3675c368085de3e9b58f96a91b544168
tecwds <tecwds@163.com>
2024-12-07 20:39:02 +0800

    add: 商品相关接口 1. 获得所有商品信息 2. 获取商品详情 3. 添加商品（批量）

---
+++ market-plus/src/main/java/top/wpaint/marketplus/entity/vo/GoodsVO.java
@@ -0,0 +1,21 @@
+package top.wpaint.marketplus.entity.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoodsVO implements Serializable {
+    private BigInteger id;
+    private String name;
+    private String description;
+    private String image;
+    private BigInteger price;
+}
