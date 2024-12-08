da501c1fe18e3c0f8ccc7b84c75086d85befd48f
tecwds <tecwds@163.com>
2024-12-08 16:34:03 +0800

    fix: 查询订单

--- market-plus/src/main/java/top/wpaint/marketplus/service/impl/OrderServiceImpl.java
+++ market-plus/src/main/java/top/wpaint/marketplus/service/impl/OrderServiceImpl.java
@@ -1,21 +1,15 @@
 package top.wpaint.marketplus.service.impl;
 
 import cn.dev33.satoken.stp.StpUtil;
-import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
-import org.springframework.context.annotation.Bean;
-import top.wpaint.marketplus.common.Status;
 import top.wpaint.marketplus.common.constant.LogicConst;
-import top.wpaint.marketplus.common.exception.AppException;
-import top.wpaint.marketplus.entity.Address;
 import top.wpaint.marketplus.entity.Goods;
 import top.wpaint.marketplus.entity.Order;
 import top.wpaint.marketplus.entity.dto.OrderDTO;
 import top.wpaint.marketplus.entity.support.OrderGoods;
-import top.wpaint.marketplus.entity.table.OrderTableDef;
+import top.wpaint.marketplus.entity.vo.GoodsVO;
 import top.wpaint.marketplus.entity.vo.OrderVO;
 import top.wpaint.marketplus.mapper.GoodsMapper;
 import top.wpaint.marketplus.mapper.OrderMapper;
@@ -23,14 +17,12 @@ import top.wpaint.marketplus.service.OrderService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 /**
- *  服务层实现。
+ * 服务层实现。
  *
  * @author tecwds
  * @since 2024-11-19
@@ -77,18 +69,40 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     @Override
     public List<OrderVO> doGetOrderList() {
         // TODO 需要优化
-        // 获得用户所有订单
-//        List<Order> oList = list(QueryWrapper.create()
-//                .where(OrderTableDef.ORDER.USER_ID.eq(StpUtil.getExtra("userId").toString())));
-//
-//        List<BigInteger> goodsIds = new ArrayList<>(oList.size());
-//        oList.forEach(it -> goodsIds.add(it.getGoodsId()));
-//
-//        List<Goods> goods = goodsMapper.selectListByIds(goodsIds);
-//
-//        List<OrderVO> voList = new ArrayList<>(oList.size());
+        // 个人所有订单
+        List<Order> orders = list();
 
-        return null;
+        // 订单 + 商品信息
+        List<OrderVO> voList = new ArrayList<>();
+
+        Set<String> goodsIDs = new HashSet<>();
+        orders.forEach(it -> goodsIDs.add(it.getGoodsId().toString()));
+
+        // 获得订单 map, 方便后续赋值
+        Map<BigInteger, Goods> abouts = goodsMapper.selectListByIds(goodsIDs)
+                .stream()
+                .collect(Collectors.toMap(Goods::getId, it -> it));
+
+        // 订单ID，订单
+        orders.stream()
+                .collect(Collectors.groupingBy(Order::getOrderId))
+                .forEach((key, value) -> {
+                    List<GoodsVO> goodsVOS = value.stream().map(it -> {
+                        // 单个商品
+                        GoodsVO vo = new GoodsVO();
+                        vo.setId(it.getGoodsId());
+                        vo.setName(abouts.get(it.getGoodsId()).getName());
+                        vo.setDescription(abouts.get(it.getGoodsId()).getDescription());
+                        vo.setImage(abouts.get(it.getGoodsId()).getImage());
+                        vo.setPrice(it.getPrice());
+                        vo.setCount(it.getCount());
+                        vo.setTotal(it.getPrice().multiply(BigInteger.valueOf(it.getCount())));
+                        return vo;
+                    }).toList();
+                    voList.add(new OrderVO(key, goodsVOS));
+                });
+
+        return voList;
 
     }
 }
